22a0c79aaed631511d88fe76f8790d7c8a643037
Seb <s.dieunidou@gmail.com>
2012-05-24 13:37:56 +0200

    add clicky

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -50,6 +50,9 @@
       <a href="<?php echo url_for('@server_list_game?slug=counter-strike-go') ?>" title="serveur tf2">Counter-Strike: GO</a> -
       <br />
       © 2011 - 2012 csgo-serv.fr - Tous droits réservés
+        <script src="//static.getclicky.com/js" type="text/javascript"></script>
+        <script type="text/javascript">try{ clicky.init(66590164); }catch(e){}</script>
+        <noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/66590164ns.gif" /></p></noscript>
     </div>
 
 </body>
